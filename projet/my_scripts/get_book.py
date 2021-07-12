886aa4bf3ca2bd9bc695742db9dd99e9d8b2aa36
tech'cian <40168247+Chreno972@users.noreply.github.com>
2021-07-12 15:47:43 +0100

    Update get_book.py

--- projet/my_scripts/get_book.py
+++ projet/my_scripts/get_book.py
@@ -5,7 +5,7 @@ import os.path
 import time
 import the_functions as tf
 from pathlib import Path
-
+import subprocess as sb
 #################################################################
 
 # instanciate route to avoid confusions
@@ -25,7 +25,7 @@ except:
 recherche = input("Mettez ici l'url du livre que vous rechercez => ")
 
 # from this condition, launch the program or not
-if "https://books.toscrape.com/catalogue/" in recherche:
+if "https://books.toscrape.com/catalogue/" in recherche and not "category" in recherche:
     url = recherche
     os.chdir(test_folder)
 
@@ -34,10 +34,12 @@ if "https://books.toscrape.com/catalogue/" in recherche:
     print(
         "Parfait, votre livre vient d'être téléchargé, rendez-vous dans le dossier 'downloads/books' pour le consulter\n"
     )
-
+    time.sleep(3)
+    os.chdir('../../../')
+    sb.call('py menu_principal.py 1', shell=True) 
 else:
     # If the given url doesn't fit the condition above, write this and break
     print(
-        "par mesures de sécurité, Seule une url provenant du site 'bookstoscrape.com' est autorisée, veuillez recommencer\n"
+        "par mesures de sécurité, Seule une url correspondant à un livre et provenant du site 'bookstoscrape.com' est autorisée, veuillez recommencer\n"
     )
     time.sleep(3)
