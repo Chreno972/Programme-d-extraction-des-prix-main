dd0aeec4b2487a0801f16c38c90f0f8c805cd0bd
tech'cian <40168247+Chreno972@users.noreply.github.com>
2021-07-12 22:35:50 +0100

    Update get_book.py

--- projet/my_scripts/get_book.py
+++ projet/my_scripts/get_book.py
@@ -8,11 +8,11 @@ from pathlib import Path
 import subprocess as sb
 #################################################################
 
-# instanciate route to avoid confusions
+# instancie le dossier qui contiendra les livres téléchargées
 base_dir = Path(os.getcwd())
 test_folder = base_dir / "downloads/books"
 
-# Create the csv file and the images folder (try/except, for the next times we download another book)
+# crée le fichier csv qui contiendra les informations du livre
 try:
     os.chdir(test_folder)
     os.mkdir("images")
@@ -21,15 +21,13 @@ try:
 except:
     pass
 
-# Here we get the user command
 recherche = input("Mettez ici l'url du livre que vous rechercez => ")
 
-# from this condition, launch the program or not
+# condition pour lancer la suite du script
 if "https://books.toscrape.com/catalogue/" in recherche and not "category" in recherche:
     url = recherche
     os.chdir(test_folder)
 
-    # Take the given url then write the choosen informations coming from it into the csv file and the images folder
     tf.write_infos(tf.book_infos_from_cat_url(url))
     print(
         "Parfait, votre livre vient d'être téléchargé, rendez-vous dans le dossier 'downloads/books' pour le consulter\n"
@@ -38,7 +36,6 @@ if "https://books.toscrape.com/catalogue/" in recherche and not "category" in re
     os.chdir('../../../')
     sb.call('py menu_principal.py 1', shell=True) 
 else:
-    # If the given url doesn't fit the condition above, write this and break
     print(
         "par mesures de sécurité, Seule une url correspondant à un livre et provenant du site 'bookstoscrape.com' est autorisée, veuillez recommencer\n"
     )
